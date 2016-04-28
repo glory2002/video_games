8d5b32ce69edfd3e60ccc4b1c55d03164c6e9374
glory <glory.angelova@gmail.com>
2016-04-28 17:11:25 +0200

    project in progress

---
+++ src/Category.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Category extends React.Component {
+  render() {
+    return (
+      <div className="category">
+        <h1>Category!</h1>
+        <p>ID: {this.props.params.categoryId}</p>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+export default Category;
